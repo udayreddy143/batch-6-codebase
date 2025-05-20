258c7c1ec603173013f2a3d6549ac245ee06d36c
sairam12335 <singlesai225@gmail.com>
2025-05-20 16:53:01 +0530

    sai updated

--- coreModule/coreModule/src/main/java/com/jashwin/coreModule/service/EmployeeService.java
+++ coreModule/coreModule/src/main/java/com/jashwin/coreModule/service/EmployeeService.java
@@ -1,16 +1,19 @@
 package com.jashwin.coreModule.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmployeeService {
+@Value("${core.name}")
+   private String name;
 
    public  EmployeeService(){
         System.out.println("this is employee Service ");
     }
 
     public String display(){
-
+        System.out.println("demo session by " + name);
         System.out.println("very good demo session by soujanya");
         return " hey my name is sudee welcome to git branch";
     }
