82b187bdea096fc7dc07633c0d0b6e33af751ed2
INE12393403 <m.uday@tesco.com>
2025-06-04 20:55:44 +0530

    spring mvc example

---
+++ springmvc-maven/src/main/java/org/jaswincodecraft/controller/MvcExample.java
@@ -0,0 +1,47 @@
+package org.jaswincodecraft.controller;
+
+import org.jaswincodecraft.model.Student;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MvcExample {
+
+    @GetMapping
+    @RequestMapping("/thisisfirstrequest")
+    public String m1(@RequestParam(value = "age",required = false) String age,@RequestParam("id") int id){
+        System.out.println(age+"--->"+"--->"+id);
+        return "hey this is getMapping" ;
+    }
+
+        @GetMapping
+        @RequestMapping("/thisissecondrequest")
+    public String m2(@RequestParam("age") String age,@RequestParam("id") int id,@RequestParam("name") String name){
+        System.out.println(age+"--->"+name+"--->"+id);
+        return "hey this is getMapping" + name;
+    }
+
+//    @GetMapping("/{age}/{id}/{name}")
+//    public String m1(@PathVariable("age") String age, @PathVariable("id") int id, @PathVariable("name") String name){
+//        System.out.println(age+"--->"+name+"--->"+id);
+//        return "hey this is getMapping" + name;
+//    }
+
+//    @PostMapping
+//    public String m2(@RequestBody Student student){
+//        System.out.println(student);
+//        return "hey this is  PostMapping";
+//    }
+//
+//    @DeleteMapping
+//    public String m3(){
+//        return "hey this is  deleteMapping";
+//    }
+
+}
---
+++ springmvc-maven/src/main/java/org/jaswincodecraft/model/Student.java
@@ -0,0 +1,61 @@
+package org.jaswincodecraft.model;
+
+public class Student {
+
+    private String name;
+    private int id;
+    private String address;
+    private int age;
+    private String skill;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", address='" + address + '\'' +
+                ", age=" + age +
+                ", skill='" + skill + '\'' +
+                '}';
+    }
+}
